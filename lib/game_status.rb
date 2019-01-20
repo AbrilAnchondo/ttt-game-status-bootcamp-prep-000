d04386497bd0187b6721c8a85542c7f6bf7d8020
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-01-20 21:23:09 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -62,7 +62,7 @@ WIN_COMBINATIONS = [
    if !won?(board)
      return nil
     else
-      if won?(board) && won?(board).all? {|i| i = "X"}
+      if won?(board).all? {|i| i = "X"}
        return "X"
        elsif
        won?(board) && won?(board).all? {|i| i = "O"}
